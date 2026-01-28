1653bf98ff24b662b8cfbc6b4933a3f78bebe2ea
RavinduLakshanMalawalaArachchi <ravindulakshan011@gmail.com>
2026-01-28 14:45:16 +0530

    Login page App bar

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gymmart/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GymMartLogin());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
